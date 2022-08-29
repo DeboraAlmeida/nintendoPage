384724cc113ac3268f777bcd60da8f00906ac6a6
DeboraAlmeida <65206552+DeboraAlmeida@users.noreply.github.com>
2022-08-29 20:28:07 -0300

    Add files via upload
    When I first listened about DOM it was really fun to play with pokéball colors. This part was developed in 2020 a month before the whole webpage and I always wanted to make them both part of the same project. Decided to do this now even though it's not improved yet.

---
+++ PokemonPage/pokemon.js
@@ -0,0 +1,19 @@
+function selectMaster() {
+	document.getElementById("myBall").setAttribute("class", "masterball");
+	
+}
+
+function selectUltra() {
+	document.getElementById("myBall").setAttribute("class", "ultraball");
+	
+}
+
+function selectPoke() {
+	document.getElementById("myBall").setAttribute("class", "pokeball");
+	
+}
+
+function selectGreat() {
+	document.getElementById("myBall").setAttribute("class", "greatball");
+	
+}
